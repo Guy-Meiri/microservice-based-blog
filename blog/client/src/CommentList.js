19f63423b04832e0184aea9fe0adb519351859db
Guy Meiri <guymeiri312@gmail.com>
2021-06-11 12:59:54 +0300

    posts yaml

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,5 +1,15 @@
 const CommentList = (props) => {
-  const commentsList = Object.values(props.comments).map((com) => <li key={com.id}>{com.content}</li>);
+  const commentsList = Object.values(props.comments).map((com) => {
+    let content;
+    if (com.status === "approved") {
+      content = com.content;
+    } else if (com.status === "pending") {
+      content = "This comment is awaiting moderation";
+    } else if (com.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+    return <li key={com.id}>{content}</li>;
+  });
 
   return <ul>{commentsList}</ul>;
 };
